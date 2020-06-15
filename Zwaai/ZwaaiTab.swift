a4713bc8c6b9531af6108dd0f6f9bf19458fa2ad
Stefan van den Oord <stefan.van.den.oord@philips.com>
2020-06-15 22:40:51 +0200

    Adds basic qr code scanner view

--- Zwaai/ZwaaiTab.swift
+++ Zwaai/ZwaaiTab.swift
@@ -1,14 +1,18 @@
 import SwiftUI
+import QRCodeReader
 
 struct ZwaaiTab: View {
     var body: some View {
-        Text("Zwaai")
-            .font(.title)
-            .tabItem {
-                VStack {
-                    Image("zwaai-tab")
-                    Text("Zwaai")
-                }
+        VStack {
+            Text("Zwaai")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("zwaai-tab")
+                        Text("Zwaai")
+                    }
+            }
+            QRView()
         }
     }
 }
@@ -16,5 +20,37 @@ struct ZwaaiTab: View {
 struct ZwaaiTab_Previews: PreviewProvider {
     static var previews: some View {
         TabView() { ZwaaiTab() }
+    }
+}
+
+struct QRView: UIViewControllerRepresentable {
+    @State var scannerDelegate: ScannerDelegate = ScannerDelegate()
+
+    func makeUIViewController(context: Context) -> QRCodeReaderViewController {
+        let builder = QRCodeReaderViewControllerBuilder {
+            $0.reader = QRCodeReader(metadataObjectTypes: [.qr], captureDevicePosition: .back)
+
+            $0.showTorchButton = false
+            $0.showSwitchCameraButton = false
+            $0.showCancelButton = false
+            $0.showOverlayView = true
+            $0.rectOfInterest = CGRect(x: 0.2, y: 0.2, width: 0.6, height: 0.6)
+        }
+        let scanner = QRCodeReaderViewController(builder: builder)
+        scanner.delegate = scannerDelegate
+        return scanner
+    }
+
+    func updateUIViewController(_ uiViewController: QRCodeReaderViewController, context: Context) {
+    }
+}
+
+class ScannerDelegate: QRCodeReaderViewControllerDelegate {
+    func reader(_ reader: QRCodeReaderViewController, didScanResult result: QRCodeReaderResult) {
+        print("did scan result", result.value)
+    }
+
+    func readerDidCancel(_ reader: QRCodeReaderViewController) {
+        print("did cancel")
     }
 }
