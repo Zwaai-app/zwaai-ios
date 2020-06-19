0595093c8c0e5435534f7af8d42b83d18b675b64
Stefan van den Oord <stefan.van.den.oord@philips.com>
2020-06-19 09:47:07 +0200

    Defines default text color

--- Zwaai/Colors.swift
+++ Zwaai/Colors.swift
@@ -2,5 +2,6 @@ import UIKit
 
 extension UIColor {
     static var appTint = UIColor(red: 0, green: 0x96/0xff, blue: 0x88/0xff, alpha: 1)
+    static var text = UIColor(red: 0x21/0xff, green: 0x25/0xff, blue: 0x29/0xff, alpha: 1)
     static var background = UIColor(white: 0xf0/0xff, alpha: 1)
 }
