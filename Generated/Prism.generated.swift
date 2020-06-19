1cc02bca72f0d091a2a7560eee6deab4d11baf69
Stefan van den Oord <stefan.van.den.oord@philips.com>
2020-06-19 14:27:19 +0200

    Use sourcery for generating action enum properties

---
+++ Generated/Prism.generated.swift
@@ -0,0 +1,36 @@
+// Generated using Sourcery 0.18.0 — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
+
+
+extension AppAction {
+    internal var history: HistoryAction? {
+        get {
+            guard case let .history(associatedValue0) = self else { return nil }
+            return (associatedValue0)
+        }
+        set {
+            guard case .history = self, let newValue = newValue else { return }
+            self = .history(newValue)
+        }
+    }
+
+    internal var isHistory: Bool {
+        self.history != nil
+    }
+
+    internal var zwaai: ZwaaiAction? {
+        get {
+            guard case let .zwaai(associatedValue0) = self else { return nil }
+            return (associatedValue0)
+        }
+        set {
+            guard case .zwaai = self, let newValue = newValue else { return }
+            self = .zwaai(newValue)
+        }
+    }
+
+    internal var isZwaai: Bool {
+        self.zwaai != nil
+    }
+
+}
--- Zwaai/Redux/AppActions.swift
+++ Zwaai/Redux/AppActions.swift
@@ -1,17 +1,5 @@
+// sourcery: Prism
 enum AppAction {
     case history(HistoryAction)
-}
-
-extension AppAction {
-    public var history: HistoryAction? {
-        get {
-            guard case let .history(value) = self else { return nil }
-            return value
-        }
-        set {
-            guard case .history = self, let newValue = newValue else { return }
-            self = .history(newValue)
-        }
-    }
     case zwaai(ZwaaiAction)
 }
