66d74ee31fb027651ddddf7dbed44261243701de
Stefan van den Oord <stefan.van.den.oord@philips.com>
2020-06-22 13:19:49 +0200

    Adds some about-style info to settings tab

--- Zwaai/Settings/SettingsHost.swift
+++ Zwaai/Settings/SettingsHost.swift
@@ -9,8 +9,47 @@ class SettingsHost: UIHostingController<SettingsTab> {
 
 struct SettingsTab: View {
     var body: some View {
-        List { Text("TODO") }
-            .listStyle(GroupedListStyle())
-            .navigationBarTitle("Instellingen")
+        List {
+            Section(header: Text("Over Zwaai")) {
+                HStack {
+                    Text("Ga naar")
+                    Button(action: { browseToSite() }) {
+                        Text("https://zwaai.app")
+                    }
+                    Text("in de browser")
+                }
+                HStack {
+                    Text("App versie")
+                    Spacer()
+                    Text(verbatim: appVersion())
+                }
+            }
+            #if DEBUG
+            Section(header: Text("Developer")) {
+                Button(action: {}) {
+                    Text("Reset app state")
+                }
+                .foregroundColor(Color(.systemRed))
+            }
+            #endif
+        }
+        .listStyle(GroupedListStyle())
+        .navigationBarTitle("Instellingen")
     }
+}
+
+struct SettingsHost_Previews: PreviewProvider {
+    static var previews: some View {
+        return TabView { SettingsTab() }
+    }
+}
+
+func browseToSite() {
+    guard let url = URL(string: "https://zwaai.app") else { return }
+    UIApplication.shared.open(url)
+}
+
+func appVersion() -> String {
+    return Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
+        ?? "?"
 }
