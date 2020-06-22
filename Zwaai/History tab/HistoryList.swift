208aa2b2205f1787ad773726aec06a98ad29f89f
Stefan van den Oord <stefan.van.den.oord@philips.com>
2020-06-22 15:54:08 +0200

    Moves type to separate file

---
+++ Zwaai/History tab/HistoryList.swift
@@ -0,0 +1,45 @@
+import SwiftUI
+
+struct HistoryList: View {
+    @Binding var history: [HistoryItem]
+
+    var dateFormatter: DateFormatter = {
+        var dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .medium
+        return dateFormatter
+    }()
+
+    var body: some View {
+        List {
+            Section(header: PersonenHeader()) {
+                ForEach(history.filter({$0.type == .person})) {item in
+                    Text(self.dateFormatter.string(from: item.timestamp))
+                }
+            }
+            Section(header: RoomsHeader()) {
+                ForEach(history.filter({$0.type == .room})) {item in
+                    Text(self.dateFormatter.string(from: item.timestamp))
+                }
+            }
+        }
+    }
+}
+
+private struct PersonenHeader: View {
+    var body: some View {
+        HStack {
+            Image(systemName: "person.fill")
+            Text("Gezwaaid met personen").font(.subheadline)
+        }
+    }
+}
+
+private struct RoomsHeader: View {
+    var body: some View {
+        HStack {
+            Image(systemName: "mappin.and.ellipse")
+            Text("Gezwaaid bij ruimtes").font(.subheadline)
+        }
+    }
+}
--- Zwaai/History tab/HistoryTab.swift
+++ Zwaai/History tab/HistoryTab.swift
@@ -45,50 +45,6 @@ struct HistoryTab_Previews: PreviewProvider {
 }
 #endif
 
-struct PersonenHeader: View {
-    var body: some View {
-        HStack {
-            Image(systemName: "person.fill")
-            Text("Gezwaaid met personen").font(.subheadline)
-        }
-    }
-}
-
-struct RoomsHeader: View {
-    var body: some View {
-        HStack {
-            Image(systemName: "mappin.and.ellipse")
-            Text("Gezwaaid bij ruimtes").font(.subheadline)
-        }
-    }
-}
-
-struct HistoryList: View {
-    @Binding var history: [HistoryItem]
-
-    var dateFormatter: DateFormatter = {
-        var dateFormatter = DateFormatter()
-        dateFormatter.dateStyle = .medium
-        dateFormatter.timeStyle = .medium
-        return dateFormatter
-    }()
-
-    var body: some View {
-        List {
-            Section(header: PersonenHeader()) {
-                ForEach(history.filter({$0.type == .person})) {item in
-                    Text(self.dateFormatter.string(from: item.timestamp))
-                }
-            }
-            Section(header: RoomsHeader()) {
-                ForEach(history.filter({$0.type == .room})) {item in
-                    Text(self.dateFormatter.string(from: item.timestamp))
-                }
-            }
-        }
-    }
-}
-
 struct UnlockButton: View {
     @ObservedObject var viewModel: ObservableViewModel<HistoryViewModel.ViewAction, HistoryViewModel.ViewState>
 
