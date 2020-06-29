b4078a63ec65550655bc7ec5fef8ee8bdd634a1a
Stefan van den Oord <stefan.van.den.oord@philips.com>
2020-06-29 08:20:15 +0200

    Removes the `Group`, is no longer needed

--- Zwaai/Zwaai tab/ZwaaiPerson.swift
+++ Zwaai/Zwaai tab/ZwaaiPerson.swift
@@ -16,30 +16,28 @@ struct ZwaaiPerson: View {
     }
 
     var body: some View {
-        Group {
-            VStack {
-                GeometryReader { imageGeo in
-                    Image(uiImage: self.qr(size: imageGeo.size))
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .shadow(radius: 2)
-                        .accessibility(label: Text("QR code die de ander moet scannen"))
-                        .accessibility(sortPriority: 1)
-                }
+        VStack {
+            GeometryReader { imageGeo in
+                Image(uiImage: self.qr(size: imageGeo.size))
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .shadow(radius: 2)
+                    .accessibility(label: Text("QR code die de ander moet scannen"))
+                    .accessibility(sortPriority: 1)
+            }
 
-                Text("Richt de telefoons naar elkaar toe, met de voorkant naar elkaar. Het scannen is gelukt zodra één van beide telefoons gaat trillen of het geluid hoorbaar is.") // swiftlint:disable:this line_length
-                    .accessibility(sortPriority: 2)
-                    .foregroundColor(Color(.text))
+            Text("Richt de telefoons naar elkaar toe, met de voorkant naar elkaar. Het scannen is gelukt zodra één van beide telefoons gaat trillen of het geluid hoorbaar is.") // swiftlint:disable:this line_length
+                .accessibility(sortPriority: 2)
+                .foregroundColor(Color(.text))
 
-                ScannerWithMask(role: .person)
-                    .padding([.leading, .trailing], 100)
+            ScannerWithMask(role: .person)
+                .padding([.leading, .trailing], 100)
 
-            }.padding()
-                .accessibilityElement(children: .contain)
-        }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color(.background))
-        .navigationBarTitle("Persoon")
+        }.padding()
+            .accessibilityElement(children: .contain)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color(.background))
+            .navigationBarTitle("Persoon")
     }
 }
 
