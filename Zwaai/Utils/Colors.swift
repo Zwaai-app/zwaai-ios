a49e8f1ac5a11cac83b8539f6f7ce85516d65d11
Stefan van den Oord <stefan.van.den.oord@philips.com>
2020-06-29 10:25:37 +0200

    Speeds up unit testing by not launching the simulator

--- Zwaai/Utils/Colors.swift
+++ Zwaai/Utils/Colors.swift
@@ -4,5 +4,6 @@ extension UIColor {
     static var appTint = UIColor(named: "appTint")!
     static var text = UIColor.secondaryLabel
     static var background = UIColor.systemBackground
-    static var zwaaiLogoBg = UIColor(named: "zwaaiLogoBg")!
+    static var zwaaiLogoBg = UIColor(named: "zwaaiLogoBg")
+        ?? UIColor(white: 0xf9/0xff, alpha: 1) // asset doesn't work in unit test
 }
