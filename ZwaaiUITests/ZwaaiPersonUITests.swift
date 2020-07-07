f0f6cae7fbd24e26dfb3dd656e42977c7082254a
Stefan van den Oord <stefan.van.den.oord@philips.com>
2020-07-07 08:54:50 +0200

    Reactivates other UI test

--- ZwaaiUITests/ZwaaiPersonUITests.swift
+++ ZwaaiUITests/ZwaaiPersonUITests.swift
@@ -6,34 +6,34 @@ class ZwaaiPersonUITests: XCTestCase {
         continueAfterFailure = false
     }
 
-//    func testZwaaiPersonAddsHistoryEntry() throws {
-//        let app = XCUIApplication()
-//        app.launch()
-//
-//        app.dismissStateWasResetWarning()
-//        app.resetState()
-//
-//        // Check history
-//        app.buttons["Geschiedenis"].tap()
-//        expect(app.tables.firstMatch.staticTexts["Gezwaaid met niemand"].exists).to(beTrue())
-//        expect(app.tables.firstMatch.staticTexts["Nergens gezwaaid"].exists).to(beTrue())
-//        expect(app.tables.firstMatch.cells.staticTexts.count) == 0
-//
-//        // Zwaai
-//        app.buttons["Zwaai"].tap()
-//        app.buttons["Zwaai met persoon"].tap()
-//        expect(app.images["QR code die de ander moet scannen"].exists).to(beTrue())
-//        expect(app.staticTexts.element(boundBy: 0).label) == "Persoon" // nav title
-//        expect(app.staticTexts.element(boundBy: 1).label).to(contain("Richt de telefoons"))
-//        expect(app.otherElements["Camera voorvertoning voor scannen"].exists).to(beTrue())
-//        app.buttons["Fake successful scan"].tap()
-//        _ = app.alerts.firstMatch.waitForExistence(timeout: 1)
-//        expect(app.alerts.firstMatch.staticTexts["Gelukt"].exists).to(beTrue())
-//        app.alerts.firstMatch.buttons["Volgende"].tap()
-//
-//        // Check history
-//        app.buttons["Geschiedenis"].tap()
-//        expect(app.tables.firstMatch.staticTexts["Gezwaaid met 1 persoon"].exists).to(beTrue())
-//        expect(app.tables.firstMatch.cells.staticTexts.count) == 1
-//    }
+    func testZwaaiPersonAddsHistoryEntry() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        app.dismissStateWasResetWarning()
+        app.resetState()
+
+        // Check history
+        app.buttons["Geschiedenis"].tap()
+        expect(app.tables.firstMatch.staticTexts["Gezwaaid met niemand"].exists).to(beTrue())
+        expect(app.tables.firstMatch.staticTexts["Nergens gezwaaid"].exists).to(beTrue())
+        expect(app.tables.firstMatch.cells.staticTexts.count) == 0
+
+        // Zwaai
+        app.buttons["Zwaai"].tap()
+        app.buttons["Zwaai met persoon"].tap()
+        expect(app.images["QR code die de ander moet scannen"].exists).to(beTrue())
+        expect(app.staticTexts.element(boundBy: 0).label) == "Persoon" // nav title
+        expect(app.staticTexts.element(boundBy: 1).label).to(contain("Richt de telefoons"))
+        expect(app.otherElements["Camera voorvertoning voor scannen"].exists).to(beTrue())
+        app.buttons["Fake successful scan"].tap()
+        _ = app.alerts.firstMatch.waitForExistence(timeout: 1)
+        expect(app.alerts.firstMatch.staticTexts["Gelukt"].exists).to(beTrue())
+        app.alerts.firstMatch.buttons["Volgende"].tap()
+
+        // Check history
+        app.buttons["Geschiedenis"].tap()
+        expect(app.tables.firstMatch.staticTexts["Gezwaaid met 1 persoon"].exists).to(beTrue())
+        expect(app.tables.firstMatch.cells.staticTexts.count) == 1
+    }
 }
