d3275fa8c77fb3ce6d08f785a255044989bb741c
Stefan van den Oord <stefan.van.den.oord@philips.com>
2020-07-14 14:32:17 +0200

    Extracts method

--- ZwaaiView/TabController.swift
+++ ZwaaiView/TabController.swift
@@ -15,17 +15,22 @@ class TabController: UITabBarController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         cancellable = appStore().statePublisher.sink { appState in
-            guard let tabItem = self.tabBar.items?.first(where: { (item) -> Bool in
-                item.tag == ViewTags.settingsTab.rawValue
-            }) else { return }
-            let hasDiscrepancy = appState.settings.notificationPermission == .allowed
-                && appState.meta.systemNotificationPermission != .authorized
-            tabItem.badgeValue = hasDiscrepancy ? "" : nil
+            self.setSettingsTabBadge(appState: appState)
         }
     }
 
     override func viewWillDisappear(_ animated: Bool) {
         cancellable?.cancel()
         super.viewWillDisappear(animated)
+    }
+
+    func setSettingsTabBadge(appState: AppState) {
+        guard let tabItem = self.tabBar.items?.first(where: {
+            $0.tag == ViewTags.settingsTab.rawValue
+        }) else { return }
+
+        let hasDiscrepancy = appState.settings.notificationPermission == .allowed
+            && appState.meta.systemNotificationPermission != .authorized
+        tabItem.badgeValue = hasDiscrepancy ? "" : nil
     }
 }
