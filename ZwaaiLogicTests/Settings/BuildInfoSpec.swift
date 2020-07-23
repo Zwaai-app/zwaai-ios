21f7ba08c90b6bc46acaab48d9a6ec8fc8d2e9fa
Stefan van den Oord <stefan.van.den.oord@philips.com>
2020-07-23 14:45:09 +0200

    Also updates test for allowed branch names

--- ZwaaiLogicTests/Settings/BuildInfoSpec.swift
+++ ZwaaiLogicTests/Settings/BuildInfoSpec.swift
@@ -8,7 +8,7 @@ class BuildInfoSpec: QuickSpec {
             let buildInfo = BuildInfo()
             expect(buildInfo.commitHash.count).toEventually(equal(7))
             expect(Data(hexEncoded: "0" + buildInfo.commitHash)).toNot(beNil())
-            expect(buildInfo.branch).to(equal("master")
+            expect(buildInfo.branch).to(equal("main")
                 || equal("develop")
                 || beginWith("feature/")
                 || beginWith("spike/"))
